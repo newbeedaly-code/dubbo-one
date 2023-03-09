fefa908ce5373f24ccd37f99ee053abcfc9fb84d
newbeedaly <newbeedaly@163.com>
2023-03-09 17:30:04 +0800

    initial

---
+++ src/main/java/cn/newbeedaly/one/DubboOneApplication.java
@@ -0,0 +1,13 @@
+package cn.newbeedaly.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboOneApplication.class, args);
+    }
+
+}
